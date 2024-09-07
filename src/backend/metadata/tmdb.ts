bef6f40c89199344d89b825df5d60d77c512e373
asakura42 <51290160+asakura42@users.noreply.github.com>
2024-09-07 15:25:53 +0000

    Update tmdb.ts

--- src/backend/metadata/tmdb.ts
+++ src/backend/metadata/tmdb.ts
@@ -144,8 +144,7 @@ export function decodeTMDBId(
 }
 
 const tmdbBaseUrl1 = "https://api.themoviedb.org/3";
-const tmdbBaseUrl2 =
-  "https://verceltmdbproxy.vercel.app/3";
+const tmdbBaseUrl2 = "https://verceltmdbproxy.vercel.app/3";
 
 const apiKey = conf().TMDB_READ_API_KEY;
 
